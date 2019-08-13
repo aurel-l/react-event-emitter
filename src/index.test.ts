d320874dc895bb2b2b69c86c4c68f5e90db92544
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-08-13 10:54:10 +0200

    added way to wait for a promise before sending an event

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,17 +1,58 @@
 import { createElement } from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, act } from '@testing-library/react';
+import { sleep, frame } from 'timing-functions';
 
 import EventEmitter from '.';
 
 afterEach(cleanup);
 
 describe('<EventEmitter />', () => {
-  test('should render and fire event', () => {
-    const listener = jest.fn();
+  let listener;
+
+  beforeEach(() => {
+    listener = jest.fn();
     document.body.addEventListener('load', listener);
+  });
 
-    render(createElement(EventEmitter, { eventType: 'load', bubbles: true }));
+  afterEach(() => {
+    document.body.removeEventListener('load', listener);
+  });
 
+  test('should render and fire event', async () => {
+    act(() => {
+      render(
+        createElement(EventEmitter, {
+          eventType: 'load',
+          bubbles: true,
+        }),
+      );
+    });
+
+    expect(listener).toHaveBeenCalledTimes(0);
+
+    await act(() => frame());
+    expect(listener).toHaveBeenCalledTimes(1);
+  });
+
+  test('should render and not fire event until wait is over', async () => {
+    const promise = sleep(100);
+
+    act(() => {
+      render(
+        createElement(EventEmitter, {
+          eventType: 'load',
+          bubbles: true,
+          waitUntil: promise,
+        }),
+      );
+    });
+
+    await act(() => frame());
+    expect(listener).toHaveBeenCalledTimes(0);
+
+    await act(() => promise);
+
+    await act(() => frame());
     expect(listener).toHaveBeenCalledTimes(1);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,14 @@ import {
   ReactNode,
   Props,
   ReactHTML,
+  useState,
 } from 'react';
 
 interface EventEmitterProps {
   children?: ReactNode;
   eventType: string;
   detail?: any;
+  waitUntil?: Promise<any>;
   bubbles?: boolean;
   cancelable?: boolean;
   composed?: boolean;
@@ -22,6 +24,7 @@ const EventEmitter = ({
   children = null,
   eventType,
   detail,
+  waitUntil = Promise.resolve(),
   bubbles,
   cancelable,
   composed,
@@ -30,14 +33,20 @@ const EventEmitter = ({
 }: EventEmitterProps) => {
   const domRef = useRef<HTMLDivElement>(null);
 
+  const [isWaiting, setIsWaiting] = useState(true);
+
+  useEffect(() => {
+    waitUntil.then(() => setIsWaiting(false));
+  }, [waitUntil]);
+
   // send or resend an event according to props
   useEffect(() => {
-    if (!domRef.current) return;
+    if (!domRef.current || isWaiting) return;
     // send a DOM event
     domRef.current.dispatchEvent(
       new CustomEvent(eventType, { detail, bubbles, cancelable, composed }),
     );
-  }, [eventType, detail, bubbles, cancelable, composed]);
+  }, [isWaiting, eventType, detail, bubbles, cancelable, composed]);
 
   // render a container to dispatch the DOM event from
   return createElement(
